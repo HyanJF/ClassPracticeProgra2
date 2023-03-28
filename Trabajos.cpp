ab2eed4d1eeebda0309eb95d609d3f60f31d9817
HyanF <hyan.fierros@uniat.edu.mx>
2023-03-28 08:46:09 -0600

    Segundo Commit

--- Trabajos.cpp
+++ Trabajos.cpp
@@ -1,7 +1,42 @@
+// Cal_Practice_Progra2.cpp : Este archivo contiene la funcion "main". La ejecucion del programa comienza y termina ahi;
+//
 #include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//vector<string> myStuff = { "Espada", "Martillo", "Bomba" };
+	//vector<string> inventory(10);
+	//vector<string> inventory(10, " - ");
+	//vector<string> inventory(myStuff);
+
+	vector<string> inventory;
+
+	inventory.push_back("Espada");
+	inventory.push_back("Escudo");
+	inventory.push_back("Martillo");
+
+	cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+
+	//Iteration FOR
+	for (unsigned int i = 0; i < inventory.size(); i++)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "\nCambiaste tu " << inventory[0] << " por una bomba." << "\n";
+	inventory[0] = "Bomba";
+
+	//Iteration DO-WHILE;
+	int i = 0;
+	do
+	{
+		cout << inventory[i] << endl;
+		i++;
+	} while (i < inventory.size());
 }
 
