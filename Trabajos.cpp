bc9fb8587583253f189924b519a590f32170e968
H.F <hyanfierros@uniat.edu.mx>
2023-05-02 08:15:15 -0600

    Nueva entrada

--- Trabajos.cpp
+++ Trabajos.cpp
@@ -7,14 +7,12 @@
 
 using namespace std;
 
-void Tarea1();
-
 int main()
 {
-	switch (switch_on)
+	switch (true)
 	{
 	case1:
-		Tarea1();
+
 	case2:
 
 	default:
@@ -51,9 +49,72 @@ int main()
 		cout << inventory[i] << endl;
 		i++;
 	} while (i < inventory.size());
-}
 
-void Tarea1()
-{
+	cout << inventory[0] << " tiene " << inventory[0].size() << " Letras en el. ";
+
+	cout << endl << "Perdiste un item en la ultima batalla" << endl;
+	inventory.pop_back();
+
+	cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+	//Iteration FOR
+	for (unsigned int i = 0; i < inventory.size(); i++)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "Un enemigo te ha robado todas tus items" << endl;
+	inventory.clear();
+
+	if (inventory.empty())
+	{
+		cout << "No tienes nada" << endl;
+	}
+	else
+	{
+		cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+		//Iteration FOR
+		for (unsigned int i = 0; i < inventory.size(); i++)
+		{
+			cout << inventory[i] << endl;
+		}
+	}
+
+	vector<string> Items;
+	int Espacio = 0;
+
+	cout << endl;
+	cout << "Agrega la cantidad de elementos que quieres para tu inventario" << endl;
+	cin >> Espacio;
+	Items.resize(Espacio);
+
+	for (unsigned int i = 0; i < Espacio; i++)
+	{
+		cout << "Agrega los items de tu inventario" << endl;
+		cin >> Items[i];
+		cout << "Tienes espacio para " << Espacio - 1 << endl;
+	}
+	cout << "Tienes " << Items.size() << " items en tu inventario" << endl;
+	//Iteration FOR
+	for (unsigned int i = 0; i < Items.size(); i++)
+	{
+		cout << Items[i] << endl;
+	}
+
+	cout << endl;
+
+	if (Items.empty())
+	{
+		cout << "No tienes nada" << endl;
+	}
+	else
+	{
+		cout << "Tienes " << Items.size() << " items en tu inventario" << endl;
+		//Iteration FOR
+		for (unsigned int i = 0; i < Items.size(); i++)
+		{
+			cout << Items[i] << endl;
+		}
+	}
+
 
 }
